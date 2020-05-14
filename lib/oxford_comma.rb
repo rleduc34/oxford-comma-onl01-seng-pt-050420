1bc7a6c309a22be23c66a1ccc854012c28e31241
Robert Leduc <rleduc34@gmail.com>
2020-05-14 04:31:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
   array.join(",")
   array.join(" and ")
-  return array.join (" and ") if array.size = 3
+  if array.length > 2
+      array.join(" and ")
+  else
+    array.join
+end
 end
